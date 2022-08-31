9f550eee581ed4dd615d05eac893e974625b4971
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-08-31 03:03:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -543,7 +543,7 @@ int main(int argc, char** argv)
     printf("----\n");
     printf("Cyberdyne Intelligence\n");
     printf("----\n");
-    printf("James William Fletcher (james@voxdsp.com)\n");
+    printf("James William Fletcher (github.com/mrbid)\n");
     printf("----\n");
     printf("Argv(2): msaa, maxfps\n");
     printf("e.g; ./uc 16 60\n");
